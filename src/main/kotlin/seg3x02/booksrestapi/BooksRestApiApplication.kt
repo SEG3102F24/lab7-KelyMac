61a1fc1fc58132a8cca8539e11a92c53cacbe1ad
Jemma <90709315+KelyMac@users.noreply.github.com>
2024-11-03 17:35:30 -0500

    initial commit

---
+++ src/main/kotlin/seg3x02/booksrestapi/BooksRestApiApplication.kt
@@ -0,0 +1,26 @@
+package seg3x02.booksrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@SpringBootApplication
+class BooksRestApiApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200") // 允许的前端地址
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                    .allowedHeaders("*")
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<BooksRestApiApplication>(*args)
+}
